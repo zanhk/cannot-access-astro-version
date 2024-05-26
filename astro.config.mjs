00cd060f7e76fd69f3385e78b93ff8a431c962dd
Giulio Zanchetta <zank@outlook.it>
2024-05-26 15:50:57 +0200

    Updated site URL and simplified dependencies
    - Changed the site URL in astro.config.mjs
    - Removed unnecessary dependencies from package.json
    - Deleted CONTRIBUTING.md, LICENSE, BlogLayout.astro, styles.css, and tailwind.config.mjs files
    - Simplified [slug].astro file content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import cloudflare from "@astrojs/cloudflare";
 import markdoc from "@astrojs/markdoc";
 import { defineConfig } from "astro/config";
 
-import robotsTxt from "astro-robots-txt";
-
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ideal-7m3.sites.dev",
+	site: "https://test.sites.dev",
 	integrations: [markdoc()],
 	output: "hybrid",
 	adapter: cloudflare(),
