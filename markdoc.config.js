cd5989bab4d62a8d19d6610a88cae9f27fc05393
Giulio Zanchetta <zank@outlook.it>
2024-05-26 15:46:29 +0200

    Refactor markdoc.config and remove unused components
    The markdoc.config file has been refactored for cleaner code structure. The 'blockLatex' and 'inlineLatex' tags have been removed from the configuration.
    
    Additionally, several unused Astro components including BlockLatex, HeaderLink, InlineLatex, and TableOfContents have been deleted. These changes also involved removing references to these components in other files such as [slug].astro under posts directory.
    
    This commit simplifies the project structure by eliminating unnecessary elements and improves readability of the configuration file.

--- markdoc.config.js
+++ markdoc.config.js
@@ -1,31 +1,18 @@
-import { defineMarkdocConfig, component, nodes } from "@astrojs/markdoc/config";
+import { component, defineMarkdocConfig, nodes } from "@astrojs/markdoc/config";
 import shiki from "@astrojs/markdoc/shiki";
 
 export default defineMarkdocConfig({
-  nodes: {
-    heading: {
-      ...nodes.heading,
-    },
-  },
-  tags: {
-    blockLatex: {
-      render: component("@components/BlockLatex.astro"),
-      attributes: {
-        formula: { type: String, required: true },
-      },
-    },
-    inlineLatex: {
-      render: component("@components/InlineLatex.astro"),
-      attributes: {
-        formula: { type: String, required: true },
-      },
-    },
-  },
-  extends: [
-    shiki({
-      langs: ["java", "python", "bash", "latex"],
-      wrap: true,
-      theme: "github-dark",
-    }),
-  ],
+	nodes: {
+		heading: {
+			...nodes.heading,
+		},
+	},
+	tags: {},
+	extends: [
+		shiki({
+			langs: ["java", "python", "bash", "latex"],
+			wrap: true,
+			theme: "github-dark",
+		}),
+	],
 });
