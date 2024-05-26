9071a6251631049f09bfcbf284ecb9d28a159182
Giulio Zanchetta <zank@outlook.it>
2024-05-26 15:53:15 +0200

    Simplified markdoc configuration and removed unused components
    In this update, we've streamlined the markdoc configuration by removing unnecessary nodes and extensions. We also deleted several files that were no longer in use, including the Header component and a couple of content posts. A new test post was added for future testing purposes.

--- markdoc.config.js
+++ markdoc.config.js
@@ -1,18 +1,6 @@
 import { component, defineMarkdocConfig, nodes } from "@astrojs/markdoc/config";
-import shiki from "@astrojs/markdoc/shiki";
 
 export default defineMarkdocConfig({
-	nodes: {
-		heading: {
-			...nodes.heading,
-		},
-	},
 	tags: {},
-	extends: [
-		shiki({
-			langs: ["java", "python", "bash", "latex"],
-			wrap: true,
-			theme: "github-dark",
-		}),
-	],
+	extends: [],
 });
