00012539fc14135d5884c7a44ed36c70a5cef511
Giulio Zanchetta <zank@outlook.it>
2024-05-26 15:45:05 +0200

    Remove unused files

--- src/utils/data/dataCpa.ts
+++
@@ -1,8 +0,0 @@
-export const materiaCPA = [
-    { label: "Álgebra", value: "Algebra" },
-    { label: "Aritmética", value: "Aritmetica" },
-    { label: "Física", value: "Fisica" },
-    { label: "Geometría Analítica", value: "Geometria" },
-    { label: "Programación", value: "Programacion" },
-    { label: "Trigonometría", value: "Trigonometria" }
-];
